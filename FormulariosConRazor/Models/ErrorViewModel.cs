e39f5eb3a72e133a24589eeaf9a24561afa32637
asus i5 <asus i5@LAPTOP-8P5LJE23>
2024-04-01 13:34:27 -0600

    Agregar archivos de proyecto.

---
+++ FormulariosConRazor/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace FormulariosConRazor.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
